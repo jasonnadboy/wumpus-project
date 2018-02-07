2382123d38ed02ac8c44760fa6bc1fb6b1e20e9c
Jason Nadboy <jason_nadboy@brown.edu>
2018-02-07 14:40:15 +0000

    Character
    Created character with get location abilities and basic move ability.

---
+++ src/gridCave/Character.java
@@ -0,0 +1,36 @@
+package gridCave;
+
+public class Character {
+	
+	private int x;
+	private int y;
+	
+	public Character(int startX, int startY) {
+		x = startX;
+		y = startY;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void move(String direction) {
+		switch(direction) {
+			case "NORTH": y--;
+				break;
+			case "SOUTH": y++;
+				break;
+			case "EAST": x--;
+				break;
+			case "WEST": x++;
+				break;
+			default:
+				break;
+		}	
+	}
+	
+}
