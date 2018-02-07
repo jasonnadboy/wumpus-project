e01404cfa9489567503200d36f214296b98ffdbf
Jason Nadboy <jason_nadboy@brown.edu>
2018-02-07 12:01:44 +0000

    edit

--- src/gridCave/test.java
+++ src/gridCave/test.java
@@ -16,6 +16,15 @@ public class test {
 	public String getCavernToCavernDirection(){
 		Cave cave = new Cave();
 		
+		int x1 = cave.getCavernX(cavernOrigin);
+		int y1 = cave.getCavernY(cavernOrigin);
+		int x2 = cave.getCavernX(cavernDestination);
+		int y2 = cave.getCavernY(cavernDestination);
+		
+		return findCavernDirection(x1,x2,y1,y2);
+	}
+
+	private String findCavernDirection(int originX, int originY, int destinationX, int destinationY) {
 		
 		return null;
 	}
