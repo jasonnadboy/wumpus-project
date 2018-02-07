725ec976cd3779685d8a975514df4c5ba96a26b2
mamaddox <mariaddox@yahoo.com>
2018-02-07 12:08:06 +0000

    test edit

--- src/gridCave/test.java
+++ src/gridCave/test.java
@@ -25,8 +25,23 @@ public class test {
 	}
 
 	private String findCavernDirection(int originX, int originY, int destinationX, int destinationY) {
-		
-		return null;
+		var xDifference = destinationX - originX;
+		var yDifference = destinationY - originY;
+
+		if(xDifference == 1 && yDifference == 0) {
+			return "E";
+		}
+		else if (xDifference == -1 && yDifference == 0) {
+			return "W";
+		}
+		else if (xDifference == 0 && yDifference == 1) {
+			return "N";
+		}
+		else if (xDifference == 0 && yDifference == -1) {
+			return "S";
+		} else {
+			return null;
+		}
 	}
 
 }
