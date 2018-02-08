4442e08c0edfb845c4c4e9a3c92ef6adef6ede2c
Jason Nadboy <jason_nadboy@brown.edu>
2018-02-08 11:47:50 +0000

    edit

--- src/test/main.java
+++ src/test/main.java
@@ -4,8 +4,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TestBats t = new TestBats();
-		System.out.println(t.dropLocationIsEvenlyDistributed());
+		//TestBats t = new TestBats();
+		TestWumpusRandomStartingLocation t = new TestWumpusRandomStartingLocation();
+		System.out.println(t.wumpusStartingPointIsRandomlyDistributed());
+		
+		TestWumpusRandomMovement t2 = new TestWumpusRandomMovement();
+		System.out.println(t2.eventDistributionOfWumpusLocationAfter1000Moves());
 		
 	}
 
