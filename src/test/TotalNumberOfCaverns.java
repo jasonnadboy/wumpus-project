75fe705ddde27516b67aa5ccc99d19ab64a8f4b6
mamaddox <mariaddox@yahoo.com>
2018-02-08 09:31:10 +0000

    totalnumberofcaverns

--- src/test/TotalNumberOfCaverns.java
+++ src/test/TotalNumberOfCaverns.java
@@ -7,11 +7,23 @@ public class TotalNumberOfCaverns {
 	
 	private Cave caverns = new Cave();
 	
-	public int totalNumberOfCavernsIs() {
+	public boolean totalNumberOfCavernsIs(int numberOfCaverns) {
 		Cavern[][] cave = caverns.getCave();
 		int length = cave.length;
 		int width = cave[0].length;
-		return (length*width);
+		for(int x = 0; x < length; x++) {
+			for(int y = 0; y < width; y++) {
+				if(cave[x][y]==null) {
+					return false;
+				}
+			}
+		}
+		if(length*width == numberOfCaverns) {
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 }
